058cb705dc6eef2637be1518104081349abf1e32
thomas <thomas.se.wagner@tum.de>
2024-03-17 06:25:49 +0100

    first frontend integration

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -2,6 +2,18 @@ import { Button, Modal, notification } from "antd";
 import React, { useState } from "react";
 import { useSDK } from "@metamask/sdk-react";
 import { Typography } from "antd";
+import { FhenixClient, generatePermit } from 'fhenixjs';
+import { Contract, Eip1193Provider, ethers } from 'ethers';
+import type { SupportedProvider, HardhatEthersProvider } from "fhenixjs";
+import { BrowserProvider } from "ethers";
+
+
+type ExtendedProvider = SupportedProvider & {
+  getTransactionReceipt(txHash: string): Promise<ethers.TransactionReceipt>;
+  send(method: string, params: any[] | Record<string, any>): Promise<any>;
+  getSigner(): Promise<any>;
+  getBalance(address: string): Promise<any>;
+};
 
 
 const Navbar: React.FC = () => {
@@ -9,12 +21,45 @@ const Navbar: React.FC = () => {
 
   const { Text, Link } = Typography;
   const [account, setAccount] = useState<string>();
-  const { sdk, connected, connecting, provider, chainId } = useSDK();
+  const { sdk, connected, connecting, chainId } = useSDK();
   const metaMaskAccount = (
     <Button shape="round" type="primary" className={"bg-primary"}>
       {account}
     </Button>
   );
+  var provider =  new BrowserProvider(window.ethereum as Eip1193Provider);
+  // const client = new FhenixClient({provider});
+
+  //const provider = new BrowserProvider(window.ethereum);
+
+
+  const abi = ["function startGame() public", "function joinGame() public", "function bet() public"]
+  //const signer = account;
+
+  //const signer = await provider.getSigner();
+
+
+  const contract = new Contract("0x0", abi);
+  //const auctionContract = new ethers.Contract(TokenContractDeployment.address, ExampleToken.abi, signer);
+
+
+  function getProvider() {
+    if (provider === null) {
+      // @ts-ignore
+      provider = new BrowserProvider(window.ethereum);
+    }
+    return provider;
+  }
+
+  //function initFHEClient() {
+  //  fheClient.value = new FhenixClient({ provider: provider as HardhatEthersProvider as SupportedProvider });
+  //}
+  
+  //function getFheClient() {
+  //  return fheClient.value;
+ // }
+
+
   const connect = async () => {
     try {
       const accounts = await sdk?.connect();
@@ -26,6 +71,48 @@ const Navbar: React.FC = () => {
       openNotification();
     }
   };
+
+  const startGame = async () => {
+    try {
+      provider = getProvider();
+      const signer = await provider.getSigner();
+      const contract = new Contract("0x82FBF91aE0093Eeac15D7941cD2a2Db6B4bbbAd2", abi, signer);
+      const tx = await contract.startGame();
+      await tx.wait();
+      console.log(tx);
+    } catch (err) {
+      console.warn("failed to start game..", err);
+      openNotification();
+    }
+  }
+
+  const joinGame = async () => {
+    try {
+      provider = getProvider();
+      const signer = await provider.getSigner();
+      const contract = new Contract("0x82FBF91aE0093Eeac15D7941cD2a2Db6B4bbbAd2", abi, signer);
+      const tx = await contract.joinGame();
+      await tx.wait();
+      console.log(tx);
+    } catch (err) {
+      console.warn("failed to join game..", err);
+      openNotification();
+    }
+  }
+
+  const bet = async () => {
+    try {
+      provider = getProvider();
+      const signer = await provider.getSigner();
+      const contract = new Contract("0x82FBF91aE0093Eeac15D7941cD2a2Db6B4bbbAd2", abi, signer);
+      const tx = await contract.joinGame();
+      await tx.wait();
+      console.log(tx);
+    } catch (err) {
+      console.warn("failed to join game..", err);
+      openNotification();
+    }
+  }
 
   const openNotification = () => {
     api.info({
@@ -41,7 +128,7 @@ const Navbar: React.FC = () => {
         <Button type={"primary"} className="text-table_black bg-white">
           {account}
         </Button>
-        <Button type={"primary"} className="text-table_black bg-white">
+        <Button type={"primary"} className="text-table_black bg-white" onClick={joinGame}>
           {account}
         </Button>
 

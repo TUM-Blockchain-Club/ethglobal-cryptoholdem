0116ca81fa629d8b636f61b4a19f8759d27fd388
Lukas Heine <lu.heine@tum.de>
2024-03-17 08:45:36 +0000

    frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import MainPage from "./components/MainPage";
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="flex flex-col">
+      <Navbar/>
       <MainPage/>
     </div>
   );

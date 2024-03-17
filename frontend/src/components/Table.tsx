03d0c5eb9d2556f88a09637a904aea8befc47c01
Daniel W <jouto@web.de>
2024-03-17 03:25:35 +0000

    poker table percentage aligned

--- frontend/src/components/Table.tsx
+++ frontend/src/components/Table.tsx
@@ -1,23 +1,31 @@
 import React, {useState} from "react"
 import cardBackground from "../CryptoHoldem_assests/Cards new/Card Background.png"
+
 import playerOne from "../CryptoHoldem_assests/Player/head-cat.png"
 import playerPlaceholder from "../CryptoHoldem_assests/Player/Placeholder Player/Placeholder Player 3.svg"
+import mainPlayer from "../CryptoHoldem_assests/Player/head-whale-alive.png"
+
 import roundTable from "../CryptoHoldem_assests/Rest/Pixel Table.png"
 const Table: React.FC = () => {
 
 
   return (
     <div className={"flex justify-center bg-primary h-[94vH]"}>
-      <div >
+      <div>
         <img src={roundTable} alt="Table" className="w-full roundTable " />
 
-        <p className={"bg-white"}></p>
+        {/*<p className={"text-white bg-cover bg-[url('/CryptoHoldem_assests/Rest/Total pot background.png')] rounded absolute top-[31%] absolute  "}>Total Pot: 200$</p>*/}
+
+        <p className={"bg-white text-primary rounded absolute top-[31%] left-[15.5%] text-2xl"}>2000$</p>
+        <p className={"bg-white text-primary rounded absolute top-[87%] right-[23%] text-2xl"}>2000$</p>
+
 
         <img src={playerOne} alt="player 1" className="playerOne" />
         <img src={playerPlaceholder} alt="player 2" className="playerTwo" />
         <img src={playerPlaceholder} alt="player 3" className="playerThree" />
         <img src={playerPlaceholder} alt="player 4" className="playerFour" />
 
+        <img src={mainPlayer} alt="mainPlayer" className="mainPlayer" />
 
         <img src={cardBackground} alt="middleCard 1" className="tableCardOne" />
         <img src={cardBackground} alt="middleCard 2" className="tableCardTwo" />
